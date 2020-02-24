5377f398df5b3856aa92f2469b444251d26531f9
Marcus A. Dahl <programkode@gmail.com>
2020-02-24 05:09:06 +0100

    prepare for event when DOM is ready

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 const API = "https://rickandmortyapi.com/api/character";
+
+document.addEventListener("ready", function () {
+
+    // Step 1: Query API and fetch payload data
+
+    // Step 2: Go through each result
+
+    // Step 3: Create elements needed to display the results
+
+    // Step 4: Add these elements to the container which holds all the results
+});
