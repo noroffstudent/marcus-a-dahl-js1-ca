831a4b38c6719debff23ba5e53abbaf1f8eb9355
Marcus A. Dahl <programkode@gmail.com>
2020-02-24 07:04:46 +0100

    fix id usage in api, create function to init container

--- js/details.js
+++ js/details.js
@@ -6,17 +6,54 @@ document.addEventListener("DOMContentLoaded", function () {
     if (params.has("id")) {
         let id = params.get("id");
 
-        fetch(`${API}id`)
+        fetch(`${API}${id}`)
             .then(function (response) {
                 return response.json();
             })
             .then(function (payload) {
                 document.querySelector(".loader").setAttribute("style", "display: none;");
-
-                // document.querySelector(".detail-container").appendChild(createDetails(payload))
+                
+                initializeDetails(payload);
             })
             .catch(function (error) {
                 window.location.href = "error.html";
             });
     }
 });
+
+
+function initializeDetails(data) {
+    const container = document.querySelector(".detail-container");
+    let image = document.createElement("img");
+
+    image.setAttribute("class", "details.image");
+    image.setAttribute("src", data.image);
+    image.setAttribute("alt", data.name);
+
+    let details = document.createElement("div");
+    details.setAttribute("class", "detail-details")
+
+    let name = document.createElement("h1");
+
+    name.innerText = data.name;
+
+    let status = document.createElement("p");
+    status.innerHTML = `Status: <span class=\"value\" id=\"status\">${data.status}</span>`;
+
+    let species = document.createElement("p");
+    species.innerHTML = `Species: <span class=\"value\" id=\"species\">${data.species}</span>`;
+
+    let origin = document.createElement("p");
+    origin.innerHTML = `Origin: <span class=\"value\" id=\"origin\">${data.origin.name}</span>`;
+
+    let location = document.createElement("p");
+    location.innerHTML = `Location: <span class=\"value\" id=\"location\">${data.location.name}</span>`;
+
+    details.appendChild(status);
+    details.appendChild(species);
+    details.appendChild(origin);
+    details.appendChild(location);
+
+    container.appendChild(image);
+    container.appendChild(details);
+}
