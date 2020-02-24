3a6d75fc8b5b0703bc4ef660459bec12b824efc6
Marcus A. Dahl <programkode@gmail.com>
2020-02-24 08:11:30 +0100

    replace words in heading and paragraphs

--- js/about.js
+++ js/about.js
@@ -1,3 +1,18 @@
-window.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(() => {
+        let heading = document.querySelector(".content h1");
+        let paragraphs = document.querySelectorAll("blockquote > :not(cite)")
 
+        // Handle heading text
+        heading.innerText = heading.innerText
+            .replace(/\sthe\s/g, " replace ")
+            .replace(/The\s/g, "Replace ");
+
+        // Handle paragraph texts
+        paragraphs.forEach(paragraph => {
+            paragraph.innerText = paragraph.innerText
+                .replace(/\sthe\s/g, " replace ")
+                .replace(/The\s/g, "Replace ");
+        });
+    }, 4000);
 });
